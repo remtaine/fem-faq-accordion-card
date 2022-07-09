ecb9838231d75fc06a494782bacbf7b0f89c5c67
Niel Benjamin Kho <benjokho@gmail.com>
2022-07-09 16:10:52 +0800

    Separate footer as component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="text-fem-neutral-300 text-center px-6 h-[2rem]">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" className='underline hover:font-bold'>Frontend Mentor</a> | Coded by <a href="https://github.com/remtaine#" className='underline hover:font-bold'>Ben</a>
+    </footer>
+  )
+}
+
+export default Footer
